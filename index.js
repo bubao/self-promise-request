701365164d57092c175f020f2f86b4aeb5e86060
bubao <asd565586630@gmail.com>
2018-11-13 14:32:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+const EventEmitter = require('events');
+const Request = require('request');
+
+class PromisRequest extends EventEmitter {
+
+	request(options) {
+		return new Promise((resolve) => {
+			const { pipe, hiden, time, size, readable, ...opts } = options;
+			const start = startNum(time);
+			let read = getRead(options);
+			let response = 0;
+			let total = 0;
+			let buffer = Buffer.alloc(0);
+			const res = Request(opts, (req) => {
+				resolve({ ...req, read, bufferBody: buffer.toString("utf8") });
+				this.removeListener("process", () => { });
+			}).on('response', (resp) => {
+				response = getLength(resp.headers['content-length'], size);
+			}).on('data', (data) => {
+				read += data.length;
+				if (readable) buffer = Buffer.concat([buffer, data]);
+				total = getTotal(size, response, read);
+				this.emit("process", {
+					completed: read, total, hiden, time: { start }, status: { down: '正在下载...', end: '完成\n' }
+				});
+			});
+			// 如果 pipe参数存在，则下载到指定路径
+			download(res, pipe);
+		});
+	}
+}
+
+function download(data, dir) {
+	if (dir && dir.length) data.pipe(fs.createWriteStream(dir || './'));
+}
+
+function getTotal(size, response, read) {
+	return (((size !== undefined || response === undefined) && size >= read) ? size : response || read + 1);
+};
+
+function startNum(time) {
+	return time !== undefined ? time.start : new Date().valueOf() / 1000
+};
+function getLength(contentLength, size) {
+	let length = contentLength || size;
+	return length ? parseInt(length || 0, 10) : 0;
+}
+
+function getRead(options) {
+	return (options.read || 0)
+};
+
+module.exports = new PromisRequest();
+
+let test = new PromisRequest();
+
+test.request({ uri: "https://www.python.org" }).then((res) => {
+	console.log("end 1", res.error)
+	test.request({ uri: "https://www.python.org" }).then(() => console.log("end2"))
+})
+test.on("process", (res) => { console.log(res.completed / res.total * 100 + "%") })
